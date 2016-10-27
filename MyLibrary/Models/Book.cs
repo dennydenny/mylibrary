44be5a478e89794cc2184120494f91916799e427
dennydenny <dennyhaliulin@gmail.com>
2016-10-27 22:34:57 +0400

    Добавлена модель книги (частично)

---
+++ MyLibrary/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyLibrary.Models
+{
+    public class Book
+    {
+        public String Name { get; private set; }
+        public DateTime Year { get; private set; }
+        public String House { get; private set; }
+        public fl
+    }
+}
