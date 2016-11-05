faf525671f78da09b7b9e201ed1bf6175660999e
dennydenny <dennyhaliulin@gmail.com>
2016-11-05 19:09:52 +0400

    Добавлены заглушки для страниц отчётов.

---
+++ MyLibrary/Controllers/ReportController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyLibrary.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View("InProgress");
+        }
+
+        public ActionResult Top10()
+        {
+            return View("InProgress");
+        }
+
+        public ActionResult StatByDay()
+        {
+            return View("InProgress");
+        }
+
+        public ActionResult InProgress()
+        {
+            return View();
+        }
+    }
+}
